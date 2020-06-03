ecedeb4b36a1a22002f100952f0be757d814bf06
Martin Guardado <martinguardado@gmail.com>
2020-06-02 19:02:09 -0600

    add db config

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+
+const database = process.env.DB_NAME;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+const sequelize = new Sequelize(database, user, password, {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+module.exports = sequelize;
