2f7553d5446486e305c055402e110dddce28dc3c
Martin Guardado <martinguardado@gmail.com>
2020-06-02 22:09:18 -0600

    add user table migration

---
+++ migrations/20200601042653-create-user-table.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("users");
+  },
+};
