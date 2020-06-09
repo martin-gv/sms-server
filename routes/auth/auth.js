e54dcd8c51d400eb5cec96083312a068e31e0182
Martin Guardado <martinguardado@gmail.com>
2020-06-08 22:07:34 -0600

    add custom authenticate to handle redirect with params

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,26 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const querystring = require("querystring");
 
 const User = require("../../models/User");
 
 router.get("/login", (req, res) => {
-  console.log("login route");
   const message = req.flash();
-  const token = req.query.token;
-  res.render("login", { message: message, token: token });
+  const tokenParam = req.query.token;
+  res.render("login", { message: message, token: tokenParam });
 });
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  (req, res) => {
-    res.redirect("/");
-  }
-);
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+
+    const tokenParam = querystring.encode({ token: req.body.token });
+
+    // Passport will return the 'info' object with a 'message' key if no
+    // credentials are supplied. The verify callback in config/passport.js
+    // will also use this 'message' key for errors. For this reason, the
+    // key 'message' should be used instead of a custom key.
+
+    if (!user) {
+      req.flash("error", info.message);
+      res.redirect("/login?" + tokenParam);
+      return;
+    }
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      res.redirect("/?" + tokenParam);
+    });
+  })(req, res, next);
+});
 
 router.get("/register", (req, res) => {
   res.render("register");
