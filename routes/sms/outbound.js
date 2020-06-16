e8f38bd390b4288063019a64a6394c4485bc730f
Martin Guardado <martinguardado@gmail.com>
2020-06-15 18:29:22 -0600

    analyze number and format in E.164

--- routes/sms/outbound.js
+++ routes/sms/outbound.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const nexmo = require("../../config/nexmo");
+const PNF = require("google-libphonenumber").PhoneNumberFormat;
+const phoneUtil = require("google-libphonenumber").PhoneNumberUtil.getInstance();
 
 const { isAuthenticated } = require("../../middleware/auth");
 
@@ -24,19 +26,44 @@ router.get("/send", (req, res) => {
 //
 
 router.post("/send", (req, res) => {
+  // Get outbound # from current user
   const from = req.user.smsNumber;
+
+  // Get message recipient and content from form submission
   const to = req.body.recipientNumber;
   const sms = req.body.messageContent;
 
-  nexmo.message.sendSms(from, to, sms, (err, nexmoRes) => {
-    if (err) {
-      req.flash("error", "Something went wrong");
-      res.redirect("/send");
+  // Validate phone number input
+  // @todo
+
+  // Check for international format
+  if (to.charAt(0) === "+") {
+    req.flash("error", `International texting currently not supported`);
+    res.redirect("/send");
+  } else {
+    const toNumber = phoneUtil.parseAndKeepRawInput(to, "CA");
+
+    // Check if it's a valid number for the region
+    if (phoneUtil.isValidNumber(toNumber)) {
+      // The leading + is removed because Nexmo documentation shows examples without
+      // it, and also states that numbers in E.164 format 'omit ... a leading +'
+      const formattedNumber = phoneUtil.format(toNumber, PNF.E164).substring(1);
+
+      // Send text message
+      nexmo.message.sendSms(from, formattedNumber, sms, (err, nexmoRes) => {
+        if (err) {
+          req.flash("error", "Something went wrong");
+          res.redirect("/send");
+        } else {
+          req.flash("success", "Message sent successfully!");
+          res.redirect("/send");
+        }
+      });
     } else {
-      req.flash("success", "Message sent successfully!");
+      req.flash("error", `<strong>${to}</strong> is not a valid phone number`);
       res.redirect("/send");
     }
-  });
+  }
 });
 
 //
