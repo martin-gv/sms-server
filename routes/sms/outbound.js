43e4678ac5540b83aaefe750520723a5948a355c
Martin Guardado <martinguardado@gmail.com>
2020-06-28 10:17:00 -0600

    make logic flow clearer

--- routes/sms/outbound.js
+++ routes/sms/outbound.js
@@ -49,37 +49,38 @@ router.post("/send", (req, res) => {
 
     // Check for international format
     if (to.charAt(0) === "+") {
-      req.flash("error", `International texting currently not supported`);
+      req.flash("error", `International texting not supported`);
       res.redirect("/send");
-    } else {
-      const toNumber = phoneUtil.parseAndKeepRawInput(to, "CA");
+      return;
+    }
 
-      // Check if it's a valid number for the region
-      if (phoneUtil.isValidNumber(toNumber)) {
-        // The leading + is removed because Nexmo documentation shows examples without
-        // it, and also states that numbers in E.164 format 'omit ... a leading +'
-        const formattedNumber = phoneUtil
-          .format(toNumber, PNF.E164)
-          .substring(1);
+    const toNumber = phoneUtil.parseAndKeepRawInput(to, "CA");
+    const isValidNumber = phoneUtil.isValidNumber(toNumber);
 
-        // Send text message
-        nexmo.message.sendSms(from, formattedNumber, sms, (err, nexmoRes) => {
-          if (err) {
-            req.flash("error", "Something went wrong");
-            res.redirect("/send");
-          } else {
-            req.flash("success", "Message sent successfully!");
-            res.redirect("/send");
-          }
-        });
+    // Check if it's a valid number for the region
+    if (isValidNumber === false) {
+      req.flash(
+        "error",
+        `<strong>${to}</strong> is not a valid Canadian phone number`
+      );
+      res.redirect("/send");
+      return;
+    }
+
+    // The leading + is removed because Nexmo documentation shows examples without
+    // it, and also states that numbers in E.164 format 'omit ... a leading +'
+    const formattedNumber = phoneUtil.format(toNumber, PNF.E164).substring(1);
+
+    // Send text message
+    nexmo.message.sendSms(from, formattedNumber, sms, (err, nexmoRes) => {
+      if (err) {
+        req.flash("error", "Something went wrong");
+        res.redirect("/send");
       } else {
-        req.flash(
-          "error",
-          `<strong>${to}</strong> is not a valid Canadian phone number`
-        );
+        req.flash("success", "Message sent successfully!");
         res.redirect("/send");
       }
-    }
+    });
   } catch (error) {
     req.flash("error", error.message);
     res.redirect("/send");
