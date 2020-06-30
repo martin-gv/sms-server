62a3b993d50c881351457860ecb68c865e812f42
Martin Guardado <martinguardado@gmail.com>
2020-06-29 22:42:56 -0600

    add handling for registration errors

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -29,37 +29,53 @@ router.get("/register", (req, res) => {
 // ─── REGISTER HANDLER ───────────────────────────────────────────────────────────
 //
 
-router.post("/register", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+router.post("/register", async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
 
-  // Check if email format is valid
-  if (!validator.validate(email)) {
-    req.flash("error", "Email is invalid");
-    res.redirect("/register" + "?email=" + email);
-    return;
-  }
-
-  if (password !== confirmPassword) {
-    req.flash("error", "Passwords don't match");
-    res.redirect("/register" + "?email=" + email);
-    return;
-  }
-
-  const saltRounds = 12;
-  const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-  const newUser = await User.create({ email: email, password: hashedPassword });
-
-  req.login(newUser, function (err) {
-    if (err) {
-      next(err);
+    // Check if email format is valid
+    if (!validator.validate(email)) {
+      req.flash("error", "Email is invalid");
+      res.redirect("/register" + "?email=" + email);
       return;
     }
 
-    req.flash("success", "Successfully registered!");
-    res.redirect("/new-number");
-  });
+    // Check if user account already exists
+    const existingUser = await User.findOne({ where: { email: email } });
+    if (existingUser !== null) {
+      req.flash("error", "An account with that email already exists");
+      res.redirect("/register" + "?email=" + email);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      req.flash("error", "Passwords don't match");
+      res.redirect("/register" + "?email=" + email);
+      return;
+    }
+
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    const newUser = await User.create({
+      email: email,
+      password: hashedPassword,
+    });
+
+    req.login(newUser, function (err) {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      req.flash("success", "Successfully registered!");
+      res.redirect("/new-number");
+    });
+  } catch (error) {
+    req.flash("error", error.message);
+    res.redirect("/register");
+  }
 });
 
 module.exports = router;
