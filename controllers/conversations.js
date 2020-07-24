804b704e312d7e454b54957a8fa1f94ff7ebba04
Martin Guardado <martinguardado@gmail.com>
2020-07-23 19:18:25 -0600

    add conversation handlers

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -1,7 +1,34 @@
-exports.getConversation = (req, res) => {
-    
-}
+const db = require("../config/database");
 
-exports.addConversation = (req, res) => {
+const Conversation = db.models.Conversation;
 
-}
+//
+// ─── GET REQUEST - RENDER CONVERSATIONS PAGE ────────────────────────────────────
+//
+
+exports.getConversation = async (req, res) => {
+  // Find all conversations for the current user. The
+  // current user instance is saved in req by Passport.
+  const User = req.user;
+  const conversations = await User.getConversations();
+
+  res.render("conversations-list", { conversations: conversations });
+};
+
+//
+// ─── POST REQUEST - HANDLE NEW CONVERSATION FORM SUBMISSION ─────────────────────
+//
+
+exports.addConversation = async (req, res) => {
+  // Prepare new record
+  const userId = req.user.id;
+  const contactPhoneNumber = req.body.phoneNumber;
+
+  // Create new record
+  await Conversation.create({
+    userId: userId,
+    contactPhoneNumber: contactPhoneNumber,
+  });
+
+  res.redirect("/conversations");
+};
