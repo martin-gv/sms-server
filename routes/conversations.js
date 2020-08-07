f23b6a2b3869a1ea379c2c00d78fcb7642b3515a
Martin Guardado <martinguardado@gmail.com>
2020-08-06 19:29:05 -0600

    change to double quotation marks

--- routes/conversations.js
+++ routes/conversations.js
@@ -6,6 +6,6 @@ const controller = require("../controllers/conversations");
 router.get("/", controller.getConversation);
 router.post("/", controller.validateNumber, controller.addConversation);
 
-router.get('/:conversationId', controller.getSingleConversation);
+router.get("/:conversationId", controller.getSingleConversation);
 
 module.exports = router;
