7fef4aa8e2f4d95379fbf623d73327feee1d5764
martin-gv <47133140+martin-gv@users.noreply.github.com>
2020-09-27 13:59:24 -0600

    refactor socket.io logic out of main server file

--- app.js
+++ app.js
@@ -63,53 +63,7 @@ app.use(passport.session()); // must run after app.use(session())
 const io = require("socket.io")(server);
 
 // Set up socket.io event listeners and other application features
-// require("./config/socket.io")(io)
-
-io.use((socket, next) => {
-  // socket.io  middleware takes two arguments, so Express middleware can't be passed to
-  // io.use directly since it requires three arguments. Instead, the socket.io middleware
-  // arguments are passed in to the Express middleware function (sessionConfig, in this case):
-  // * socket.handshake, like the req object, has a headers.cookie property
-  // * sessionConfig normally checks req.headers.cookie and populates req.session
-  // * in this case, sessionConfig checks socket.handshake.headers.cookie and populates socket.handshake.session
-  sessionConfig(socket.handshake, {}, next);
-});
-
-const db = require("./config/database");
-const Conversation = db.models.Conversation;
-
-// socket.io connections are established when the client visit the single conversation page.
-io.on("connect", (socket) => {
-  // This event will subscribe the client to a room using the conversation id as the name of the room
-  socket.on("conversation subscribe", async (data) => {
-    // The session property is established by socket.io middleware. If the user is already
-    // logged in, the session saved in the database will include a passport.user property
-    const session = socket.handshake.session;
-
-    // Check the session. If the user isn't logged in, send and error.
-    if (!session.passport || !session.passport.user) {
-      socket.emit("conversation error", {
-        message: "Error: not authenticated",
-      });
-      return;
-    }
-
-    // Find the conversation in the database
-    const conversation = await Conversation.findByPk(data.conversationId);
-
-    // Check the owner of the conversation. If it isn't the current user, send an error.
-    // passport.user includes the serialized user, which in this case is an id
-    if (session.passport.user !== conversation.userId) {
-      socket.emit("conversation error", {
-        message: "Error: not authorized for requested conversation",
-      });
-      return;
-    }
-
-    // Join the room if none of the error checks above were triggered
-    socket.join(data.conversationId);
-  });
-});
+require("./config/socket.io")(io);
 
 //
 // ─── EXTERNAL APP INTEGRATION - API ─────────────────────────────────────────────
