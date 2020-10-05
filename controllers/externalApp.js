ea9c2247fddceab9061b572c3f4889da42242264
martin-gv <47133140+martin-gv@users.noreply.github.com>
2020-10-04 22:07:34 -0600

    set up Twilio for external app API calls

--- controllers/externalApp.js
+++ controllers/externalApp.js
@@ -1,10 +1,10 @@
-const nexmo = require("../config/nexmo");
+const twilio = require("../config/twilio");
 
 //
 // ─── HANDLE EXTERNAL APP REQUEST ────────────────────────────────────────────────
 //
 
-exports.handleExternalAppRequest = (req, res) => {
+exports.handleExternalAppRequest = (req, res, next) => {
   // This handler function allows external apps to send text messages
   // by sending in a POST request. This allows external apps to easily
   // integrate with this application (e.g. a CRM can send text messages to contacts)
@@ -13,11 +13,16 @@ exports.handleExternalAppRequest = (req, res) => {
   const to = req.body.to;
   const sms = req.body.sms;
 
-  nexmo.message.sendSms(from, to, sms, (err, nexmoRes) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      res.status(200).json(nexmoRes);
-    }
-  });
+  twilio.messages
+    .create({
+      body: sms,
+      from: from,
+      to: to,
+    })
+    .then((message) => {
+      res.status(200).json(message);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 };
