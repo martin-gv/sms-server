dd91e0a2659dac9fa1493e4674821841f688e25a
martin-gv <47133140+martin-gv@users.noreply.github.com>
2020-10-12 12:22:44 -0600

    add try catch

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -11,69 +11,73 @@ const User = db.models.User;
 //
 
 // This controller function requires a socket.io instance
-exports.inboundMessage = (io) => async (req, res) => {
-  // Get the values of the inbound SMS from the Twilio POST request
-  const fromNumber = req.body.From;
-  const toNumber = req.body.To;
-  const messageContent = req.body.Body;
+exports.inboundMessage = (io) => async (req, res, next) => {
+  try {
+    // Get the values of the inbound SMS from the Twilio POST request
+    const fromNumber = req.body.From;
+    const toNumber = req.body.To;
+    const messageContent = req.body.Body;
 
-  // Find the account associated with the 'to' number
-  const user = await User.findOne({ where: { smsNumber: toNumber } });
+    // Find the account associated with the 'to' number
+    const user = await User.findOne({ where: { smsNumber: toNumber } });
 
-  // Find the user's conversation that matches the 'from' number
-  const matchingConversation = await Conversation.findOne({
-    where: { userId: user.id, contactPhoneNumber: fromNumber },
-  });
-
-  // This variable will save the id of either the matching conversation,
-  // or the newly created conversation if no matching one exists.
-  let conversationId;
-
-  // Save the id of the matching conversation
-  if (matchingConversation !== null) {
-    conversationId = matchingConversation.id;
-  } else {
-    // Or create a new one if no matching one exists
-    const newConversation = await Conversation.create({
-      userId: user.id,
-      contactPhoneNumber: fromNumber,
+    // Find the user's conversation that matches the 'from' number
+    const matchingConversation = await Conversation.findOne({
+      where: { userId: user.id, contactPhoneNumber: fromNumber },
     });
 
-    conversationId = newConversation.id;
-  }
+    // This variable will save the id of either the matching conversation,
+    // or the newly created conversation if no matching one exists.
+    let conversationId;
 
-  // Save the inbound message to the database and update the unread messages count
-  const message = await db.transaction(async (t) => {
-    const message = await Message.create(
-      {
-        conversationId: conversationId,
-        isInboundMessage: true,
-        messageContent: messageContent,
-      },
-      { transaction: t }
-    );
+    // Save the id of the matching conversation
+    if (matchingConversation !== null) {
+      conversationId = matchingConversation.id;
+    } else {
+      // Or create a new one if no matching one exists
+      const newConversation = await Conversation.create({
+        userId: user.id,
+        contactPhoneNumber: fromNumber,
+      });
 
-    await Conversation.increment("unreadMessages", {
-      where: { id: conversationId },
-      transaction: t,
+      conversationId = newConversation.id;
+    }
+
+    // Save the inbound message to the database and update the unread messages count
+    const message = await db.transaction(async (t) => {
+      const message = await Message.create(
+        {
+          conversationId: conversationId,
+          isInboundMessage: true,
+          messageContent: messageContent,
+        },
+        { transaction: t }
+      );
+
+      await Conversation.increment("unreadMessages", {
+        where: { id: conversationId },
+        transaction: t,
+      });
+
+      return message;
     });
 
-    return message;
-  });
+    // Emit the message via socket.io. If the user is currently connected they will see the reply
+    emitSocketIoEvent({ io: io, message: message });
 
-  // Emit the message via socket.io. If the user is currently connected they will see the reply
-  emitSocketIoEvent({ io: io, message: message });
+    // Send the recipient an email notification if they've set one up in their settings
+    const emailRecipients = user.emailNotificationRecipients;
+    if (emailRecipients !== null && emailRecipients !== "") {
+      sendEmailNotification(emailRecipients, fromNumber, message);
+    }
 
-  // Send the recipient an email notification if they've set one up in their settings
-  const emailRecipients = user.emailNotificationRecipients;
-  if (emailRecipients !== null && emailRecipients !== "") {
-    sendEmailNotification(emailRecipients, fromNumber, message);
+    // Respond to the Twilio webhook request. The code below follows the example in the Twilio docs
+    const twiml = new MessagingResponse();
+    res.writeHead(200, { "Content-Type": "text/xml" });
+    res.end(twiml.toString());
+  } catch (error) {
+    next(error);
   }
-
-  // Respond to the Twilio webhook request. The code below follows the example in the Twilio docs
-  const twiml = new MessagingResponse();
-  res.writeHead(200, { "Content-Type": "text/xml" });
-  res.end(twiml.toString());
 };
 
 //
