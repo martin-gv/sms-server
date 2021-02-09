dfd269d99004bea56193e80bc3ef1afa4bbe7b76
martin-gv <47133140+martin-gv@users.noreply.github.com>
2021-02-08 21:19:04 -0700

    add comments

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -6,7 +6,10 @@ const controller = require("../controllers/webhooks");
 // This route file accepts a socket.io instance for use in the inbound message controller
 module.exports = (io) => {
   router.post("/inbound-sms", controller.inboundMessage(io)); // Pass in the socket.io instance
+
+  // These routes do not use the socket.io instance
   router.post("/delivery-receipts", controller.deliveryReceipts);
+  router.post("/stripe-events", controller.stripeEvents)
 
   return router;
 };
