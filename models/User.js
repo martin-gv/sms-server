c0710dc6d940bd1d6bc83ef1c929b2d601ca89bd
martin-gv <47133140+martin-gv@users.noreply.github.com>
2021-02-14 09:48:50 -0700

    add Stripe fields to model

--- models/User.js
+++ models/User.js
@@ -22,6 +22,15 @@ module.exports = (sequelize) => {
       accountKey: {
         type: DataTypes.UUID,
       },
+      stripeCustomerId: {
+        type: DataTypes.TEXT,
+        unique: true,
+      },
+      subscriptionStatus: {
+        type: DataTypes.ENUM("ACTIVE", "INACTIVE"),
+        defaultValue: "INACTIVE",
+        allowNull: false,
+      },
     },
     { tableName: "Users" }
   );
