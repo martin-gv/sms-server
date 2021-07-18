f8f33052e32007076b0d2a27f361ec69b69453d5
martin-gv <47133140+martin-gv@users.noreply.github.com>
2021-07-18 14:10:25 -0600

    remove middleware

--- middleware/subscriptionStatus.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  const { subscriptionStatus } = req.user;
-
-  res.locals.hasValidSubscription =
-    subscriptionStatus === "ACTIVE" || subscriptionStatus === "PAST DUE";
-
-  next();
-};
