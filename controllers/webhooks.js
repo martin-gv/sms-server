ff2d34f1c9b10f654a5508c74841c8a39b13147c
martin-gv <47133140+martin-gv@users.noreply.github.com>
2021-07-24 15:01:06 -0600

    add stripe webhook endpoints

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -1,6 +1,7 @@
 const db = require("../config/database");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const mailgun = require("../config/mailgun");
+const stripe = require("../config/stripe");
 
 const Conversation = db.models.Conversation;
 const Message = db.models.Message;
@@ -124,4 +125,148 @@ function sendEmailNotification(emailRecipients, fromNumber, message) {
 
 exports.deliveryReceipts = (req, res) => {
   res.status(204).end();
+};
+
+//
+// ─── STRIPE EVENTS - WEBHOOK ────────────────────────────────────────────────────
+//
+
+exports.stripeEvents = async (req, res, next) => {
+  try {
+    //
+    // ─── RETRIEVE THE EVENT ──────────────────────────────────────────
+    //
+
+    let event;
+
+    const rawBody = req.rawBody;
+    const signature = req.headers["stripe-signature"];
+    const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
+
+    // Construct the event by verifying the signature using the raw body and secret
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        rawBody,
+        signature,
+        STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️ Webhook signature verification failed.`);
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Extract the object from the event.
+    const stripeObject = event.data.object;
+    const eventType = event.type;
+
+    //
+    // ─── HANDLE EVENT TYPES ──────────────────────────────────────────
+    //
+
+    switch (eventType) {
+      /**
+       * Initial payment is successful and the subscription is now created in Stripe. We can now add the
+       * Stripe customer ID to the user's record. This event also triggers for subscription reactivations.
+       */
+      case "checkout.session.completed": {
+        const checkoutSession = stripeObject;
+
+        await User.update(
+          {
+            stripeCustomerId: checkoutSession.customer,
+            subscriptionStatus: "ACTIVE",
+            hasPaymentError: false,
+          },
+          { where: { id: checkoutSession.client_reference_id } } // find by the Stripe session's client_reference_id
+        );
+
+        break;
+      }
+
+      // ─────────────────────────────────────────────────────────────────
+
+      /**
+       * Reset a user's status and payment error when a Stripe payment goes through.
+       * This resets the statuses set by the webhooks:
+       * - invoice.payment_failed
+       * - customer.subscription.updated (past due)
+       */
+      case "invoice.paid": {
+        const invoice = stripeObject;
+
+        await User.update(
+          { subscriptionStatus: "ACTIVE", hasPaymentError: false },
+          { where: { stripeCustomerId: invoice.customer } } // find the user by the Stripe customer id
+        );
+
+        break;
+      }
+
+      // ─────────────────────────────────────────────────────────────────
+
+      /**
+       * Update the user's payment error status if a Stripe payment fails
+       */
+      case "invoice.payment_failed": {
+        const invoice = stripeObject;
+
+        // Don't update when creating new subscriptions (whether for new or old customers). i.e. No errors
+        // need to be saved when a subscription isn't successfully created in a checkout session. Otherwise,
+        // this should run for failed subscription cycle payments and other failed payments.
+        if (invoice.billing_reason !== "subscription_create") {
+          await User.update(
+            { hasPaymentError: true },
+            { where: { stripeCustomerId: invoice.customer } } // find the user by the Stripe customer id
+          );
+        }
+
+        break;
+      }
+
+      // ─────────────────────────────────────────────────────────────────
+
+      /**
+       * Update the user's status after the Stripe subscription becomes past due
+       */
+      case "customer.subscription.updated": {
+        const subscription = stripeObject;
+
+        if (subscription.status === "past_due") {
+          await User.update(
+            { subscriptionStatus: "PAST DUE" },
+            { where: { stripeCustomerId: subscription.customer } } // find user by the Stripe customer id
+          );
+        }
+
+        break;
+      }
+
+      // ─────────────────────────────────────────────────────────────────
+
+      /**
+       * Update the user's status after the Stripe subscription is cancelled
+       */
+      case "customer.subscription.deleted": {
+        const subscription = stripeObject;
+
+        await User.update(
+          { subscriptionStatus: "CANCELED", hasPaymentError: false },
+          { where: { stripeCustomerId: subscription.customer } } // find user by the Stripe customer id
+        );
+
+        break;
+      }
+
+      // ─────────────────────────────────────────────────────────────────
+
+      default:
+      // Unhandled event types
+    }
+
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
 };
